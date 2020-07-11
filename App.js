b94cf7cac26b0cab6781e5951fca6319a323dd62
Anurag Sharan <sharananurag998@gmail.com>
2020-07-11 20:03:56 +0530

    Added walkthrough page

--- App.js
+++ App.js
@@ -1,8 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import AppContainer from './routes/mainRoute'; 
+import { AppLoading } from "expo";
+import * as Font from "expo-font";
+
+
 import Navigator from './routes/drawer';
 
-export default function App() {
-  return (
-    <Navigator/>
-  );
+// export default function App() {
+//   return (
+//     <Navigator/>
+//   );
+// }
+
+
+function App() {
+  const [isLoadingComplete, setLoadingComplete] = useState(false);
+  if (!isLoadingComplete) {
+    return (
+      <AppLoading
+        startAsync={loadResourcesAsync}
+        onError={handleLoadingError}
+        onFinish={() => handleFinishLoading(setLoadingComplete)}
+      />
+    );
+  } else {
+    return isLoadingComplete ? <AppContainer /> : <AppLoading />;
+  }
 }
+async function loadResourcesAsync() {
+  await Promise.all([
+    Font.loadAsync({
+      "roboto-regular": require("./assets/fonts/roboto-regular.ttf")
+    })
+  ]);
+}
+function handleLoadingError(error) {
+  console.warn(error);
+}
+
+function handleFinishLoading(setLoadingComplete) {
+  setLoadingComplete(true);
+}
+
+export default App;
---
+++ screens/SignUp.js
@@ -0,0 +1,216 @@
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  View,
+  StatusBar,
+  ImageBackground,
+  Text,
+  TextInput,
+  TouchableOpacity
+} from "react-native";
+import MaterialCommunityIconsIcon from "react-native-vector-icons/MaterialCommunityIcons";
+import EvilIconsIcon from "react-native-vector-icons/EvilIcons";
+
+function SignUp(props) {
+  return (
+    <View style={styles.root}>
+      <StatusBar barStyle="light-content" backgroundColor="rgba(0,0,0,0)" />
+      <View style={styles.background}>
+        <ImageBackground
+          style={styles.rect2}
+          imageStyle={styles.rect2_imageStyle}
+          source={require("../assets/images/Gradient_OBnqoPE.png")}
+        >
+          <View style={styles.text3Column}>
+            <Text style={styles.text3}>CREATE ACCOUNT</Text>
+            <MaterialCommunityIconsIcon
+              name="account-circle"
+              style={styles.icon8}
+            ></MaterialCommunityIconsIcon>
+            <View style={styles.form}>
+              <View style={styles.nameColumn}>
+                <View style={styles.name}>
+                  <EvilIconsIcon
+                    name="user"
+                    style={styles.icon5}
+                  ></EvilIconsIcon>
+                  <TextInput
+                    placeholder="Name"
+                    placeholderTextColor="rgba(255,255,255,1)"
+                    secureTextEntry={false}
+                    style={styles.nameInput}
+                  ></TextInput>
+                </View>
+                <View style={styles.email}>
+                  <EvilIconsIcon
+                    name="envelope"
+                    style={styles.icon6}
+                  ></EvilIconsIcon>
+                  <TextInput
+                    placeholder="Email"
+                    placeholderTextColor="rgba(255,255,255,1)"
+                    secureTextEntry={false}
+                    style={styles.emailInput}
+                  ></TextInput>
+                </View>
+              </View>
+              <View style={styles.nameColumnFiller}></View>
+              <View style={styles.password}>
+                <EvilIconsIcon name="lock" style={styles.icon7}></EvilIconsIcon>
+                <TextInput
+                  placeholder="Password"
+                  placeholderTextColor="rgba(255,255,255,1)"
+                  secureTextEntry={true}
+                  style={styles.passwordInput}
+                ></TextInput>
+              </View>
+            </View>
+          </View>
+          <View style={styles.text3ColumnFiller}></View>
+          <View style={styles.buttonColumn}>
+            <TouchableOpacity
+              onPress={() => props.navigation.navigate("Home")}
+              style={styles.button}
+            >
+              <Text style={styles.text2}>Continue</Text>
+            </TouchableOpacity>
+            <Text style={styles.text4}>Terms &amp; Conditions</Text>
+          </View>
+        </ImageBackground>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    backgroundColor: "rgba(144,19,254,1)"
+  },
+  background: {
+    flex: 1
+  },
+  rect2: {
+    flex: 1
+  },
+  rect2_imageStyle: {},
+  text3: {
+    color: "rgba(255,255,255,1)",
+    fontSize: 24,
+    alignSelf: "center"
+  },
+  icon8: {
+    color: "rgba(128,128,128,1)",
+    fontSize: 105,
+    height: 114,
+    width: 105,
+    marginTop: 28,
+    marginLeft: 87
+  },
+  form: {
+    height: 230,
+    borderRadius: 70,
+    marginTop: 36
+  },
+  name: {
+    height: 59,
+    backgroundColor: "rgba(255,255,255,0.25)",
+    borderRadius: 5,
+    flexDirection: "row"
+  },
+  icon5: {
+    color: "rgba(255,255,255,1)",
+    fontSize: 33,
+    width: 33,
+    height: 33,
+    marginLeft: 15,
+    alignSelf: "center"
+  },
+  nameInput: {
+    height: 30,
+    color: "rgba(255,255,255,1)",
+    fontSize: 14,
+    flex: 1,
+    marginRight: 17,
+    marginLeft: 13,
+    marginTop: 14
+  },
+  email: {
+    height: 59,
+    backgroundColor: "rgba(255,255,255,0.25)",
+    borderRadius: 5,
+    flexDirection: "row",
+    marginTop: 27
+  },
+  icon6: {
+    color: "rgba(255,255,255,1)",
+    fontSize: 33,
+    marginLeft: 15,
+    alignSelf: "center"
+  },
+  emailInput: {
+    height: 30,
+    color: "rgba(255,255,255,1)",
+    flex: 1,
+    marginRight: 17,
+    marginLeft: 13,
+    marginTop: 14
+  },
+  nameColumn: {},
+  nameColumnFiller: {
+    flex: 1
+  },
+  password: {
+    height: 59,
+    backgroundColor: "rgba(255,255,255,0.25)",
+    borderRadius: 5,
+    flexDirection: "row"
+  },
+  icon7: {
+    color: "rgba(255,255,255,1)",
+    fontSize: 33,
+    marginLeft: 15,
+    marginTop: 13
+  },
+  passwordInput: {
+    height: 30,
+    color: "rgba(255,255,255,1)",
+    flex: 1,
+    marginRight: 17,
+    marginLeft: 13,
+    marginTop: 14
+  },
+  text3Column: {
+    marginTop: 90,
+    marginLeft: 41,
+    marginRight: 41
+  },
+  text3ColumnFiller: {
+    flex: 1
+  },
+  button: {
+    height: 55,
+    backgroundColor: "rgba(247,247,247,0)",
+    borderRadius: 100,
+    borderColor: "rgba(255,255,255,1)",
+    borderWidth: 1,
+    justifyContent: "center",
+    marginBottom: 55
+  },
+  text2: {
+    width: 66,
+    color: "rgba(255,255,255,1)",
+    alignSelf: "center"
+  },
+  text4: {
+    color: "rgba(255,255,255,0.5)",
+    alignSelf: "center"
+  },
+  buttonColumn: {
+    marginBottom: 31,
+    marginLeft: 41,
+    marginRight: 41
+  }
+});
+
+export default SignUp;
