45ade702222ed65119768d3747f36129d7e420c0
Anurag Sharan <sharananurag998@gmail.com>
2020-07-13 03:02:56 +0530

    Updated navigation to v5
    Fixed: Header issue

--- routes/walkthroughRoutes.js
+++ routes/walkthroughRoutes.js
@@ -1,23 +1,18 @@
-import { createAppContainer } from 'react-navigation'
-import {createStackNavigator} from 'react-navigation-stack';
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
 import TestPage from '../screens/containers/TestPage';
 import Login from '../screens/Login.js';
 
-const WalkthroughNavigator = createStackNavigator(
-    {
-        TestPage         : { screen : TestPage, navigationOptions: {
-            headerShown: false,
-        } },
-        AfterTestPage    : { screen : Login, navigationOptions: {
-            headerShown: false,
-        } }
-    },
-    {
-        initialRouteName : 'TestPage',
-        navigationOptions: {
-            headerShown: false,
-        },
-    }
-)
+const Stack = createStackNavigator();
+
+function WalkthroughNavigator() {
+  return (
+    <Stack.Navigator headerMode={false}>
+      <Stack.Screen name="TestPage" component={TestPage} options= {{headerShown: false}} />
+      <Stack.Screen name="AfterTestPage" component={Login} options= {{headerShown: false}} />
+    </Stack.Navigator>
+  );
+}
 
 export default WalkthroughNavigator;
