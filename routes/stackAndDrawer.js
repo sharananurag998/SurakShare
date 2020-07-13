566f8ca26330eec6777fa1f7424c813c979b9937
Anurag Sharan <sharananurag998@gmail.com>
2020-07-13 22:29:02 +0530

    Added aboutUs and fixed transferHistory

--- routes/stackAndDrawer.js
+++ routes/stackAndDrawer.js
@@ -11,6 +11,8 @@ import {MaterialIcons} from '@expo/vector-icons';
 import Home from '../screens/home'; 
 import UserProfile from '../screens/userProfile'; 
 import ShareFiles from '../screens/shareFiles';
+import TransferHistory from '../screens/transferHistory';
+import AboutUs from '../screens/aboutUs';
 
 const Stack = createStackNavigator();
 
@@ -92,6 +94,48 @@ function ShareFilesStackNavigator({ navigation }) {
 }
 
 
+function TransferHistoryStackNavigator({ navigation }) {
+  return (
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerLeft: ()=> <NavigationDrawerStructure navigationProps={navigation} />,
+        headerStyle: {
+          backgroundColor: '#5b0a91', //Set Header color
+        },
+        headerTintColor: '#fff', //Set Header text color
+      }}>
+      <Stack.Screen 
+        name="Transfer History" 
+        component={TransferHistory} 
+        options={{
+          title: 'Transfer History', //Set Header Title
+        }}/>
+    </Stack.Navigator>
+  );
+}
+
+function AboutUsStackNavigator({ navigation }) {
+  return (
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerLeft: ()=> <NavigationDrawerStructure navigationProps={navigation} />,
+        headerStyle: {
+          backgroundColor: '#5b0a91', //Set Header color
+        },
+        headerTintColor: '#fff', //Set Header text color
+      }}>
+      <Stack.Screen 
+        name="About Us" 
+        component={AboutUs} 
+        options={{
+          title: 'About Us', //Set Header Title
+        }}/>
+    </Stack.Navigator>
+  );
+}
+
   const Drawer = createDrawerNavigator();
   
   export default function SurakShare() {
@@ -107,6 +151,11 @@ function ShareFilesStackNavigator({ navigation }) {
           <Drawer.Screen name="Home" component={HomeStackNavigator} />
           <Drawer.Screen name="Profile" component={ProfileStackNavigator} />
           <Drawer.Screen name="Share Files" component={ShareFilesStackNavigator} />
+          <Drawer.Screen name="Groups" component={TransferHistoryStackNavigator} />
+          <Drawer.Screen name="Transfer History" component={TransferHistoryStackNavigator} />
+          <Drawer.Screen name="Document Scanner" component={TransferHistoryStackNavigator} />
+          <Drawer.Screen name="About Us" component={AboutUsStackNavigator} />
+          
         </Drawer.Navigator>
       </NavigationContainer>
     );
--- screens/transferHistory.js
+++ screens/transferHistory.js
@@ -1,200 +1,91 @@
-import React, { Component } from "react";
-import { StyleSheet, View, Text } from "react-native";
-import MaterialChipWithImage3 from "../components/MaterialChipWithImage3";
-import IoniconsIcon from "react-native-vector-icons/Ionicons";
-import MaterialChipWithImage4 from "../components/MaterialChipWithImage4";
-import MaterialChipWithImage5 from "../components/MaterialChipWithImage5";
-import MaterialButtonDanger from "../components/MaterialButtonDanger";
-import MaterialCardWithImageAndTitle from "../components/MaterialCardWithImageAndTitle";
-import FontAwesomeIcon from "react-native-vector-icons/FontAwesome";
+import React from "react";
+import { StyleSheet, View, Text, FlatList } from "react-native";
+import { ListItem } from 'react-native-elements'
 
-function Untitled(props) {
+import senderImage from'../assets/images/aniketh1.jpg'
+import receiverImage from'../assets/images/aniketh1.jpg'
+
+function TransferHistory(props) {
+
+  const transfers=[
+    {
+      from:"Adarsh",
+      to:"Anurag",
+      date:"12-10-2020",
+      filename:"file.jpg"
+    },
+    {
+      from:"Aniketh",
+      to:"Anurag",
+      date:"12-10-2020",
+      filename:"file.jpg"
+    },
+  ]
+
+  const keyExtractor = (item, index) => index.toString()
+
+  const renderItem = ({ item }) => (
+    <ListItem
+      title={item.from}
+      leftAvatar={{ source: senderImage }}
+      bottomDivider
+      rightAvatar={{source: receiverImage}}
+      rightTitle={item.to}
+      subtitle={item.filename}
+      rightSubtitle={item.date}
+      rightSubtitleStyle={styles.date}
+    />
+  )
+  
+
   return (
-    <View style={styles.container}>
-      <View style={styles.materialChipWithImage3Stack}>
-        <MaterialChipWithImage3
-          style={styles.materialChipWithImage3}
-        ></MaterialChipWithImage3>
-        <Text style={styles.dateTime2}>Date, time</Text>
-        <IoniconsIcon name="ios-document" style={styles.icon2}></IoniconsIcon>
-      </View>
-      <View style={styles.materialChipWithImage4Stack}>
-        <MaterialChipWithImage4
-          style={styles.materialChipWithImage4}
-        ></MaterialChipWithImage4>
-        <Text style={styles.dateTime1}>Date, time</Text>
-        <IoniconsIcon name="ios-document" style={styles.icon3}></IoniconsIcon>
-      </View>
-      <View style={styles.materialChipWithImage5Stack}>
-        <MaterialChipWithImage5
-          style={styles.materialChipWithImage5}
-        ></MaterialChipWithImage5>
-        <Text style={styles.dateTime3}>Date, time</Text>
-        <IoniconsIcon name="ios-document" style={styles.icon4}></IoniconsIcon>
-      </View>
-      <MaterialButtonDanger
-        style={styles.materialButtonDanger}
-      ></MaterialButtonDanger>
-      <MaterialCardWithImageAndTitle
-        style={styles.materialCardWithImageAndTitle}
-      ></MaterialCardWithImageAndTitle>
-      <View style={styles.materialChipWithImage52Stack}>
-        <MaterialChipWithImage5
-          style={styles.materialChipWithImage52}
-        ></MaterialChipWithImage5>
-        <Text style={styles.dateTime4}>Date,Time</Text>
-        <FontAwesomeIcon
-          name="file-image-o"
-          style={styles.icon5}
-        ></FontAwesomeIcon>
-      </View>
+    <View>
+      <View style={styles.topRow}>
+      <Text style={styles.from}>From</Text>
+      <Text style={styles.to}>To</Text>
+    </View>
+    <FlatList
+      keyExtractor={keyExtractor}
+      data={transfers}
+      renderItem={renderItem}/>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "rgba(189,16,224,1)"
+    backgroundColor: "#FFF",
+    borderWidth: 0,
+    borderColor: "#000000"
   },
-  materialChipWithImage3: {
-    width: 330,
-    height: 70,
-    position: "absolute",
-    left: 0,
-    top: 0,
-    backgroundColor: "rgba(255,255,255,1)",
-    borderWidth: 6,
-    borderColor: "rgba(126,211,33,1)"
+  date:{
+    fontSize:12
   },
-  dateTime2: {
-    top: 25,
-    left: 190,
-    position: "absolute",
-    fontFamily: "roboto-regular",
-    color: "#121212",
-    fontSize: 17
+  topRow:{
+    flexDirection:"row",
+    backgroundColor:"#45046a"
   },
-  icon2: {
-    top: 13,
-    left: 58,
-    position: "absolute",
-    color: "rgba(128,128,128,1)",
-    fontSize: 40
+  from:{
+    flex:1,
+    fontSize:20,
+    textAlign:"center",
+    backgroundColor:"#5c2a9d",
+    borderColor:"#45046a",
+    borderWidth: 2,
+    borderRadius:10,
+    padding:10,
+    color:"#F8F8FF"
   },
-  materialChipWithImage3Stack: {
-    width: 330,
-    height: 70,
-    marginTop: 323,
-    marginLeft: 30
-  },
-  materialChipWithImage4: {
-    width: 330,
-    height: 70,
-    position: "absolute",
-    left: 0,
-    top: 0,
-    backgroundColor: "rgba(255,255,255,1)",
-    borderWidth: 6,
-    borderColor: "rgba(208,2,27,1)"
-  },
-  dateTime1: {
-    top: 25,
-    left: 190,
-    position: "absolute",
-    fontFamily: "roboto-regular",
-    color: "#121212",
-    fontSize: 17
-  },
-  icon3: {
-    top: 14,
-    left: 60,
-    position: "absolute",
-    color: "rgba(128,128,128,1)",
-    fontSize: 40
-  },
-  materialChipWithImage4Stack: {
-    width: 330,
-    height: 70,
-    marginTop: 25,
-    marginLeft: 30
-  },
-  materialChipWithImage5: {
-    width: 339,
-    height: 70,
-    position: "absolute",
-    left: 0,
-    top: 0,
-    backgroundColor: "rgba(255,255,255,1)",
-    borderWidth: 6,
-    borderColor: "rgba(126,211,33,1)"
-  },
-  dateTime3: {
-    top: 25,
-    left: 64,
-    position: "absolute",
-    fontFamily: "roboto-regular",
-    color: "#121212",
-    fontSize: 17
-  },
-  icon4: {
-    top: 13,
-    left: 239,
-    position: "absolute",
-    color: "rgba(128,128,128,1)",
-    fontSize: 40
-  },
-  materialChipWithImage5Stack: {
-    width: 339,
-    height: 70,
-    marginTop: 31
-  },
-  materialButtonDanger: {
-    height: 83,
-    width: 232,
-    borderRadius: 100,
-    overflow: "visible",
-    marginTop: 38,
-    marginLeft: 64
-  },
-  materialCardWithImageAndTitle: {
-    height: 181,
-    width: 359,
-    borderBottomRightRadius: 26,
-    borderBottomLeftRadius: 26,
-    backgroundColor: "rgba(131,38,152,1)",
-    marginTop: -686
-  },
-  materialChipWithImage52: {
-    width: 339,
-    height: 70,
-    position: "absolute",
-    left: 0,
-    top: 0,
-    backgroundColor: "rgba(255,255,255,1)",
-    borderWidth: 6,
-    borderColor: "rgba(126,211,33,1)"
-  },
-  dateTime4: {
-    top: 25,
-    left: 63,
-    position: "absolute",
-    fontFamily: "roboto-regular",
-    color: "#121212",
-    fontSize: 17
-  },
-  icon5: {
-    top: 15,
-    left: 228,
-    position: "absolute",
-    color: "rgba(128,128,128,1)",
-    fontSize: 40
-  },
-  materialChipWithImage52Stack: {
-    width: 339,
-    height: 70,
-    marginTop: 24
+  to:{
+    flex:1,
+    textAlign:"center",
+    fontSize:20,
+    backgroundColor:"#5c2a9d",
+    borderColor:"#45046a",
+    borderWidth: 2,
+    padding:10,
+    color:"#F8F8FF",
+    borderRadius:10,
   }
 });
-
-export default Untitled;
+export default TransferHistory;
