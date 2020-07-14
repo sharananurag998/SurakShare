c65ac143bea75c3e46550f71f01facb5728c9552
Anurag Sharan <sharananurag998@gmail.com>
2020-07-14 10:52:11 +0530

    updated global styles

--- styles/global.js
+++ styles/global.js
@@ -3,7 +3,7 @@ import { StyleSheet, StatusBar, Platform } from 'react-native';
 export const globalStyles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: '#fbeeff',
         alignItems: 'center',
         justifyContent: 'center',
         paddingTop: Platform.OS === "android"?StatusBar.currentHeight:0, 
