acab24892161bdafe5a9ef31dff06d0e3a6ded0f
Abdul Mateen <abdulmateen075@gmail.com>
2020-07-31 08:46:36 +0530

    Added image picker for user's profile image

--- screens/userProfile.js
+++ screens/userProfile.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 import { StyleSheet, View, StatusBar } from 'react-native';
 import SyncStorage from 'sync-storage';
 import { Avatar, Button, Text, Card, Paragraph, Title, Divider, Snackbar, TextInput, IconButton } from 'react-native-paper';
-
-const LeftContent = (props) => <Avatar.Image {...props} size={125} source={require('../assets/images/camylla-battani-zSCoQkrLMOE-unsplash.jpg')} />;
+import ImagePicker from 'react-native-image-picker';
+// More info on all the options is below in the API Reference... just some common use cases shown here
 
 export default class Home extends Component {
 	constructor(props) {
@@ -13,16 +13,51 @@ export default class Home extends Component {
 			name: SyncStorage.get('name') || 'John',
 			email: SyncStorage.get('email') || 'email not provided',
 			wallet: SyncStorage.get('wallet'),
+			fileUri: null,
 			visible: false,
 		};
 	}
+
+	imageUriOrPath = () => {
+		return this.state.fileUri ? { uri: this.state.fileUri } : require('../assets/images/camylla-battani-zSCoQkrLMOE-unsplash.jpg');
+	};
+
+	LeftContent = (props) => <Avatar.Image {...props} size={125} source={this.imageUriOrPath()} />;
+
+	chooseImage = () => {
+		const options = {
+			title: 'Select Avatar',
+			storageOptions: {
+				skipBackup: true,
+				path: 'images',
+			},
+		};
+
+		ImagePicker.showImagePicker(options, (response) => {
+			console.log('Response = ', response);
+
+			if (response.didCancel) {
+				console.log('User cancelled image picker');
+			} else if (response.error) {
+				console.log('ImagePicker Error: ', response.error);
+			} else {
+				console.log('response', JSON.stringify(response));
+				this.setState({
+					// filePath: response,
+					// fileData: response.data,
+					fileUri: response.uri,
+				});
+			}
+		});
+	};
 
 	render() {
 		return (
 			<View style={{ backgroundColor: '#fff', flex: 1 }}>
 				<Card style={{ flex: 1 }}>
-					<Card.Cover style={{ opacity: 0.35 }} source={require('../assets/images/camylla-battani-zSCoQkrLMOE-unsplash.jpg')} />
-					<Card.Title title={this.state.name} subtitle='Bangalore, IN' left={LeftContent} leftStyle={{ width: '35%' }} />
+					<Card.Cover style={{ opacity: 0.35 }} source={this.imageUriOrPath()} />
+					<Card.Title title={this.state.name} subtitle='Bangalore, IN' left={this.LeftContent} leftStyle={{ width: '35%' }} />
+					<IconButton icon='circle-edit-outline' color={'#343434'} size={30} onPress={this.chooseImage} />
 					<Card.Content style={{ marginVertical: '10%', flex: 1 }}>
 						<Title style={{ fontSize: 22 }}>Profile Settings</Title>
 					</Card.Content>
@@ -57,7 +92,7 @@ export default class Home extends Component {
 							onFocus={() => {
 								this.props.navigation.navigate('ChangePassword');
 							}}
-							value={'password'}
+							value={'change password'}
 						/>
 					</View>
 					<View style={{ alignItems: 'center', flexDirection: 'row' }}>
@@ -77,11 +112,6 @@ export default class Home extends Component {
 						</Button>
 					</View>
 				</View>
-				{/* <View style={{ marginHorizontal: '15%' }}>
-					<Snackbar visible={this.state.visible} duration={100} onDismiss={this.setState({ visible: false })}>
-						Changes saved successfully
-					</Snackbar>
-				</View> */}
 			</View>
 		);
 	}
